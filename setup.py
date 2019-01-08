35c03c605d2913b23a7284219e4ba7746dc0e794
Panagiotis Dimopoulos <panosdim@gmail.com>
2019-01-08 10:44:39 +0200

    Update Toll Values

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opass",
-    version="1.0.5",
+    version="1.0.6",
     author="Panagiotis Dimopoulos",
     author_email="panosdim@gmail.com",
     description="Calculate toll costs in Olympia Odos",
